25efd0b914ca863ded1f67b88a76b123c1e352ee
xavigh <fgargallo@gmail.com>
2019-03-21 14:24:10 +0100

    commit changes to USflights and sql queries.

--- sql_queriesUsFlights.sql
+++ sql_queriesUsFlights.sql
@@ -1,4 +1,7 @@
-SELECT 
-    COUNT(*)
-FROM
-    usflights.flights;
+
+
+/* Retard promidge de sortide i arribade segons l' aeroprt d' origen */
+SELECT Origin, AVG(ArrTime) AS "avg_ArrTime" , AVG(DepTime) as "avg_DepTime"
+FROM usflights.flights
+Group BY Origin
+
