cf966905c6e768a1146bc0035ad0e6f947455cb4
xavigh <fgargallo@gmail.com>
2019-03-27 12:29:32 +0100

    deleting and leaving only de pdf file

--- USFlights/createUser.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'testUser'@'localhost' IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON USAirlineFlights.* TO 'testUser'@'localhost';
-
-/* Refrescar privilegis */
-FLUSH PRIVILEGES;
--- sql_queriesUsFlights.sql
+++
@@ -1,7 +0,0 @@
-
-
-/* Retard promidge de sortide i arribade segons l' aeroprt d' origen */
-SELECT Origin, AVG(ArrTime) AS "avg_ArrTime" , AVG(DepTime) as "avg_DepTime"
-FROM usflights.flights
-Group BY Origin
-
